6501f543c20bf40566ddd180c6169e8fc4952bfd
Akshat jain <akshatj180@gmail.com>
2019-09-07 12:34:10 +0530

    Second change

--- akshat.cpp
+++ akshat.cpp
@@ -6,11 +6,12 @@ using namespace std;
 int main(){
 
     int a = 10;
-    
+
     cout<<"Learning git and github";
     cout<<"Basics of git and github";
     cout<<a;
 
     //This is our first version.
+    //This is second version.
     return(0);
 }
